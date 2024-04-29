e014e1c48b1e9d9b66eb3277d4a9094048019d8c
AffanDN <affan03nusli@gmail.com>
2024-04-29 08:43:13 +0700

    Initial Commit

---
+++ Animalia/AnimaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaliaApp.swift
+//  Animalia
+//
+//  Created by Macbook Pro on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
