cbd138b9c3953d7cffea1a023b76443e621243d7
AffanDN <affan03nusli@gmail.com>
2024-04-30 15:51:56 +0700

    Add photo gallery feature

--- Animalia/Main/AlbumView.swift
+++ Animalia/Main/AlbumView.swift
@@ -6,10 +6,66 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct AlbumView: View {
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var selectedImages: [UIImage] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // MARK: - DISPLAY SELECTED IMAGES
+
+            if selectedItems.isEmpty {
+                ContentUnavailableView("No Photos", systemImage: "photos.on.rectangle", description: Text("To get Started, select some photos below."))
+                    .frame(height: 300)
+            } else {
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        ForEach(selectedImages, id: \.self) {
+                            image in
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 24))
+                                .padding(.horizontal, 20)
+                                .containerRelativeFrame(.horizontal)
+                        }
+                    }
+                }//: SCROLL VIEW
+                .frame(height: 300)
+            }
+            Spacer()
+            // MARK: - PHOTOS PICKER
+
+            PhotosPicker(selection: $selectedItems, maxSelectionCount: 5, selectionBehavior: .continuousAndOrdered, matching: .images) {
+                Label("Select a Photo", systemImage: "photo")
+            }
+            .photosPickerStyle(.inline)
+//            .photosPickerDisabledCapabilities(.selectionActions)
+            .frame(height: 500)
+            .ignoresSafeArea()
+            .onChange(of: selectedItems) { oldItems, newItems in
+                print("OLD Items: \(oldItems.count)")
+                print("NEW Items: \(newItems.count)")
+                selectedImages = []
+                newItems.forEach {
+                    newItem in
+                    Task {
+                        if let data = try? await newItem.loadTransferable(type: Data.self) {
+                            if let image = UIImage(data: data) {
+                                selectedImages.append(image)
+                            }
+                        }
+                    }
+                }
+            }
+            
+        } // VSTACK
+        // Simple Implementation
+//        PhotosPicker(selection: $selectedItem, matching: .images) {
+//            Label("Select a Photo", systemImage: "photo")
+//        }
     }
 }
 
